8a0129e64b80030187fc24a5feca32f669bfb630
Kavi Gupta <kavi@berkeley.edu>
2025-04-28 15:40:18 -0400

    add compatibility with pythons up to 3.14

--- setup.py
+++ setup.py
@@ -5,18 +5,21 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ast_scope",  # Replace with your own username
-    version="0.4.6",
+    version="0.5.0",
     author="Kavi Gupta",
     author_email="ast_scope@kavigupta.org",
     description="Annotates a Python AST with the scope of symbols.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kavigupta/ast_scope",
-    packages=setuptools.find_packages('ast_scope'),
+    packages=setuptools.find_packages("ast_scope"),
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9",
